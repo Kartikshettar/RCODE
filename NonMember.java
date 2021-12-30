4050efe93c8e0d2e5c5a67e866b6f098b29163f5
Kartikshettar <86262379+Kartikshettar@users.noreply.github.com>
2021-12-30 20:28:22 +0530

    RejectedCode

---
+++ NonMember.java
@@ -0,0 +1,16 @@
+package FlightReservation;
+
+public class NonMember extends Passenger {
+
+    public NonMember(int a, String n) {
+        super(a, n);
+    }
+
+    @Override
+    public double applyDiscount(double p) {
+        if (age > 65) {
+            p = (p *9)/10;
+            return p;
+        }
+        return p;
+    }}
